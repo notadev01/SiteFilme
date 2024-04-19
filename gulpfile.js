0d6e952dfccf7af10de8b7806ef27400b3eefebb
notadev01 <notadev01@email.com>
2024-04-19 04:43:12 -0300

    Consertar processo de build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
+const replace = require('gulp-replace');
 
 function compileSass() {
     return gulp.src('./src/styles/main.scss')
@@ -10,10 +12,34 @@ function compileSass() {
         .pipe(gulp.dest('./dist/styles'))
 }
 
+function compressImages() {
+    return gulp.src('./src/images/**/*', { encoding: false })
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+}
+
+function copyFonts() {
+    return gulp.src('./assets/fonts/*.woff2')
+        .pipe(gulp.dest('./dist/assets/fonts'))
+}
+
+function replaceHTML() {
+    return gulp.src('./index.html')
+        .pipe(replace('dist/', './'))
+        .pipe(replace('src/', './'))
+        .pipe(gulp.dest('dist/'))
+}
+
+exports.compress = compressImages;
+exports.replace = replaceHTML;
+exports.copyFonts = copyFonts;
 exports.sass = compileSass;
 
-exports.default = gulp.parallel(compileSass);
+exports.default = gulp.parallel(compileSass, compressImages, copyFonts, replaceHTML);
 
-exports.watch = function() {
+exports.watch = function () {
     gulp.watch('./src/styles/*.scss', { ignoreInitial: false }, gulp.series(compileSass));
+    gulp.watch('./src/images/**/*', { ignoreInitial: false }, gulp.series(compressImages));
+    gulp.watch('./assets/fonts/*', { ignoreInitial: false }, gulp.series(copyFonts));
+    gulp.watch('./index.html', { ignoreInitial: false }, gulp.series(replaceHTML));
 }
